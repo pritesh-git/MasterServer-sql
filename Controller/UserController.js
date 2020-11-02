51b9cb7a3028caee58140ff8447b91ae702cf665
pritesh-git <pritesh.bhati59@gmail.com>
2020-11-02 14:07:17 +0530

    formatted

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const mysqlConnection = require("../Config/cnctDb")
+const mysqlConnection = require("../Config/cnctDb");
 //Get all User
 exports.GetAll = (req, res) => {
   mysqlConnection.query("SELECT * FROM users", (err, rows, fields) => {
@@ -10,7 +10,7 @@ exports.GetAll = (req, res) => {
 };
 
 //Get an User By ID
-exports.GetById = (req, res) => {  
+exports.GetById = (req, res) => {
   mysqlConnection.query(
     "SELECT * FROM users WHERE id = ?",
     [req.params.id],
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,7 +1,6 @@
 module.exports = function (app) {
   const user = require("../Controller/UserController");
 
-  // //   app.post("/login", user.LoginUser);
     app.post("/newUser", user.CreateUser);
     app.get("/users", user.GetAll);
     app.get("/users/:id", user.GetById);
